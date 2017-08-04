8d2c1e94be0e78404c34b0d9a7cbc395ecad61c0
Kilian Helmenstein <kilian.helmenstein@gmail.com>
2017-08-04 21:03:05 +0200

    Changed order of arguments (now more intuitive)

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-pub fn index_in_string_equals_to(index: &u32, string: &str, wanted_char: &char) -> bool {
+pub fn index_in_string_equals_to(string: &str, index: &u32, wanted_char: &char) -> bool {
     match string.chars().nth(*index as usize) {
         Some(actual_char) if actual_char == *wanted_char => true,
         _ => false,
