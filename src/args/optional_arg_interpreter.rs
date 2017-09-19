03497a323cb42b9cbd1c4b7b9a9411a3c7d51808
Kilian Helmenstein <kilian.helmenstein@gmail.com>
2017-09-19 13:54:36 +0200

    Began to implement the optional-argument-interpreter function

--- src/args/optional_arg_interpreter.rs
+++ src/args/optional_arg_interpreter.rs
@@ -2,13 +2,44 @@ use super::*;
 use super::extract_values::*;
 
 pub fn interprete_optional_arg<'a>(
-    stream: &'a [Token],
     name: &'static str,
-    defined_count: &Count) -> Result<(&'a [Token], Option<ArgValue>), &'static str> {
+    defined_count: &Count,
+    defined_short_name: &String,
+    defined_long_name: &String,
+    stream: &'a [Token]) -> Result<(&'a [Token], Option<ArgValue>), &'static str> {
 
     match stream[0] {
         Token::ShortName(ref short_name) => Err(""),
         Token::LongName(ref long_name) => Err(""),
         Token::Value(_) => Ok((stream, None)),
+    }
+
+    let (min, max) = match defined_count {
+        &Count::Fixed(fixed_count) => (fixed_count, fixed_count),
+        &Count::Minimum(min_count) => (min_count, 100 as usize),
+        &Count::Maximum(max_count) => (0 as usize, max_count),
+        &Count::Range { min, max } => (min, max),
+    };
+
+    let (stream, values) = n_following_values(stream[1..], min, max)?;
+    Ok((stream, actual_logical_index+1, Some(ArgValue::new(name, 1, values))))
+}
+
+#[cfg(test)]
+mod test {
+    use super::super::super::tokens;
+    use super::super::super::tokens::*;
+    use super::super::*;
+
+    #[test]
+    fn interprete_optional_arg__two_values__captures_two_values() {
+        let token_stream = tokens::tokenize(&vec![String::from("1"), String::from("2"), String::from("3"), String::from("4")]);
+
+        let (stream, maybe_value) = super::interprete_optional_arg("opional_arg", &Count::Fixed(2), &String::from("-o"), &String::from("--optional"), &token_stream).unwrap();
+        let arg_value = maybe_value.unwrap();
+
+        assert!(arg_value.occurences == 1);
+        assert!(arg_value.name == String::from("opional_arg"));
+        assert!(arg_value.assigned_values == vec![String::from("1"), String::from("2")]);
     }
 }
