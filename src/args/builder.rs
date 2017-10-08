fb2d27ac71ccc79c92dc028d3ff7d6c7232cf2a0
Kilian Helmenstein <kilian.helmenstein@gmail.com>
2017-10-08 16:05:54 +0200

    Nothin

--- src/args/builder.rs
+++ src/args/builder.rs
@@ -1,8 +1,8 @@
-use super::ArgDefinition;
-use super::Count;
+use args::Count;
+use args::ArgDefinition;
+use args::ArgValue;
+use tokens::Token;
 
-
-use super::optional_arg_interpreter;
 
 pub struct ArgDefinitionBuilder {
     name: &'static str,
@@ -11,21 +11,38 @@ pub struct ArgDefinitionBuilder {
 }
 
 impl ArgDefinitionBuilder {
-    fn Called(name: &'static str) -> Self {
+    fn called(name: &'static str) -> Self {
         ArgDefinitionBuilder {
             name: name, count: None, interprete_tokens: None
         }
     }
 
-    fn with_count(self, count: Count) -> Self {
+    fn with_count(mut self, count: Count) -> Self {
         self.count = Some(count);
         self
     }
 
-    fn on_index(self, index: usize) -> Self {
+    fn on_index(mut self, index: usize) -> Self {
         use super::positional_arg_interpreter::interprete_positional_arg;
 
-        self.interprete_tokens = |stream, current_index, name, count| interprete_positional_arg(name, count, index, stream, current_index);
+        self.interprete_tokens = Some(Box::new(
+            move |stream, current_index, name, count| interprete_positional_arg(name, count, index.clone(), stream, current_index)));
         self
+    }
+
+    fn as_option(mut self, short_name: String, long_name: String) -> Self {
+        use super::optional_arg_interpreter::interprete_optional_arg;
+
+        self.interprete_tokens = Some(Box::new(
+            move |stream, current_index, name, count| interprete_optional_arg(name, count, &short_name, &long_name, stream, current_index)));
+        self
+    }
+
+    fn assembled(mut self) -> ArgDefinition {
+        ArgDefinition {
+            name: self.name,
+            count: self.count.unwrap(),
+            interprete_tokens: self.interprete_tokens.unwrap()
+        }
     }
 }
